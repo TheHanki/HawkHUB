673c8aaae38ca8b5b4e9dc118e031c32cda4c761
TheHanki <cihansakin2007@gmail.com>
2025-08-16 21:55:10 +0300

    Update Loader.lua

--- Roblox/Main/Loader.lua
+++ Roblox/Main/Loader.lua
@@ -1,1 +1,8 @@
+local old;
+old = hookfunction(game.HttpGet, newcclosure(function(olgame, url)
+if url:find("xwerta") then
+url = url:gsub("xwerta","TheHanki")
+end
+return old(olgame, url)
+end))
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xwerta/HawkHUB/refs/heads/main/Roblox/Main/HawkHUB.lua"))()
